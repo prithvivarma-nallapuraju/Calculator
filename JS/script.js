4a54037f12fa85d1c15cb7626596d76430f269f0
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2022-10-26 10:52:28 +0530

    updated changes

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+let display = document.querySelector('.display')
+let buttons = Array.from(document.getElementsByClassName('button'))
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText) {
+            default: 
+                display.innerText += e.target.innerText
+        }
+    })
+    if(buttons.length>100) {
+        display.innerText = 'Error!'
+    }
+})
